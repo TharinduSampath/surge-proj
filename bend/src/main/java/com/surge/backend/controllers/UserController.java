fd9d749c5fc8b5c5eb254412ac16821b9cfd3de9
TharinduSampath <gunasekeratharindu@gmail.com>
2022-08-10 08:25:39 +0530

    API endpoints for serving user details created

--- bend/src/main/java/com/surge/backend/controllers/UserController.java
+++ bend/src/main/java/com/surge/backend/controllers/UserController.java
@@ -1,9 +1,24 @@
 package com.surge.backend.controllers;
 
+import com.surge.backend.services.UserService;
 import lombok.AllArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController @AllArgsConstructor @RequestMapping("/user")
 public class UserController {
+    private final UserService service;
+
+    public Page<User> getUsers (@RequestParam String status,
+                               @RequestParam(required = false) String search,
+                               @RequestParam(required = false) Integer page,
+                               @RequestParam(required = false) Integer size) {
+        Pageable pageable = PageRequest.of(page != null ? page : 0, size != null ? size: 20);
+        return service.getUsers(search, pageable, status);
+    }
 }
--- bend/src/main/java/com/surge/backend/services/UserService.java
+++ bend/src/main/java/com/surge/backend/services/UserService.java
@@ -1,10 +1,38 @@
 package com.surge.backend.services;
 
+import com.surge.backend.domains.Note;
 import com.surge.backend.repos.UserRepository;
 import lombok.AllArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor @Service
 public class UserService {
     private final UserRepository repo;
+    private final MongoTemplate template;
+
+    public Page<User> getUsers(String search, Pageable pageable, String status) {
+        Query query = new Query().with(pageable);
+        final List<Criteria> criteria = new ArrayList<>();
+
+        criteria.add(Criteria.where("status").regex(String.format("^%s$",status), "i"));
+        if (search != null && !search.isBlank()) {
+            criteria.add(Criteria.where("title").regex(search, "i"));
+        }
+        query.addCriteria(new Criteria().andOperator(criteria.toArray(new Criteria[0])));
+
+        return PageableExecutionUtils.getPage(
+                template.find(query, User.class),
+                pageable,
+                () -> template.count(query.skip(0).limit(0), User.class));
+    }
 }
